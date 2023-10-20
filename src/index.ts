535e6309f099346ca63c7c408c083b045e0b36a7
Rafael Olivares <olrafael@mailfence.com>
2023-10-20 14:57:35 -0300

    Remove api settings to leave only scraper

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,5 @@
-import bodyParser from "body-parser";
-import cors from "cors";
-import express, { Request, Response } from "express";
 import cron from "node-cron";
 
-import { client } from "./config";
 import { runScraping } from "./scrape";
 
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-
-const getMentions = (request: Request, response: Response) => {
-  client.query("SELECT * FROM mentions", (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
-};
-
-app.route("/mentions").get(getMentions);
-
 cron.schedule("0 0,6,12,18 * * *", () => runScraping());
-// cron.schedule("* * * * *", () => runScraping());
-
-app.listen(process.env.PORT || 3002, () => console.log(`Server listening`));
