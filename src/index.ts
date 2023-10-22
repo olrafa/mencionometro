a3ad91a02fb3d7c100eb5fc2070b9dfc4c88fcf6
Rafael Olivares <olrafael@mailfence.com>
2023-10-22 18:09:15 -0300

    Revert "Vercelk cron new setup"
    This reverts commit a04f1a3425329a45f1d8a4dc187cd9afd9963ab8.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express, { Request, Response } from "express";
+import cron from "node-cron";
 
-// import cron from "node-cron";
 import { client } from "./config";
-import scraper from "./scrape";
+import { runScraping } from "./scrape";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-app.use("/cron", scraper);
 
 const getMentions = (request: Request, response: Response) => {
   client.query("SELECT * FROM mentions", (error, results) => {
@@ -30,6 +28,6 @@ app.get("/", (req, res) => {
 app.route("/mentions").get(getMentions);
 
 // cron.schedule("0 0,6,12,18 * * *", () => runScraping());
-// cron.schedule("*/5 * * * *", () => runScraping());
+cron.schedule("*/5 * * * *", () => runScraping());
 
 app.listen(process.env.PORT || 3002, () => console.log(`Server listening`));
--- src/scrape.ts
+++ src/scrape.ts
@@ -47,7 +47,7 @@ const scrapeWebsiteForTerm = async (
   }
 };
 
-const scraper = async () => {
+export const runScraping = async () => {
   for (const searchTerm of SEARCH_TERMS) {
     for (const { mediaOutlet, url } of WEBSITES) {
       await scrapeWebsiteForTerm(mediaOutlet, url, searchTerm);
@@ -55,5 +55,3 @@ const scraper = async () => {
   }
   console.log("Search finished at", new Date());
 };
-
-export default scraper;
