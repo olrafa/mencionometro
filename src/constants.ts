13b0fa28161e3bb11887a8f766057812d8e1878e
Rafael Olivares <olrafael@mailfence.com>
2023-10-24 15:08:21 -0300

    "Adds more media sources"

--- src/constants.ts
+++ src/constants.ts
@@ -20,4 +20,28 @@ export const WEBSITES: Website[] = [
   { mediaOutlet: "TNT Sports", url: "https://tntsports.com.br/" },
   { mediaOutlet: "CNN Brasil", url: "https://www.cnnbrasil.com.br/" },
   { mediaOutlet: "Veja", url: "https://veja.abril.com.br/" },
+  { mediaOutlet: "Terra", url: "https://www.terra.com.br/" },
+  { mediaOutlet: "IG", url: "https://www.ig.com.br/" },
+  { mediaOutlet: "Nexo", url: "https://www.nexojornal.com.br/" },
+  { mediaOutlet: "Metrópoles", url: "https://www.metropoles.com/" },
+  { mediaOutlet: "O Dia", url: "https://odia.ig.com.br/" },
+  {
+    mediaOutlet: "Diário do Nordeste",
+    url: "https://diariodonordeste.verdesmares.com.br/",
+  },
+  { mediaOutlet: "Extra", url: "https://extra.globo.com/" },
+  {
+    mediaOutlet: "Correio Braziliense",
+    url: "https://www.correiobraziliense.com.br/",
+  },
+  { mediaOutlet: "Estado de Minas", url: "https://www.em.com.br/" },
+  { mediaOutlet: "O Povo", url: "https://www.opovo.com.br/" },
+  { mediaOutlet: "Zero Hora", url: "https://gauchazh.clicrbs.com.br/" },
+  {
+    mediaOutlet: "Diário de Pernambuco",
+    url: "https://www.diariodepernambuco.com.br/",
+  },
+  { mediaOutlet: "Quem", url: "https://revistaquem.globo.com/" },
+  { mediaOutlet: "Caras", url: "https://caras.uol.com.br/" },
+  { mediaOutlet: "Contigo", url: "https://contigo.uol.com.br/" },
 ];
