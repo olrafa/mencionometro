9b8bf664243646cb2be445a95fd6745d302d3fdc
Rafael Olivares <olrafael@mailfence.com>
2023-10-27 14:57:01 -0300

    Adds sites route

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 import home from "./routes/home";
 import mentions from "./routes/mentions";
+import sites from "./routes/sites";
 import summary from "./routes/summary";
 import timestamps from "./routes/timestamps";
 
@@ -14,6 +15,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(home, mentions, summary, timestamps);
+app.use(home, mentions, summary, timestamps, sites);
 
 app.listen(process.env.PORT || 3002, () => console.log(`Server listening`));
---
+++ src/routes/sites.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+import { client } from "../config";
+
+const router = express.Router();
+
+router.get("/sites", (request: Request, response: Response) => {
+  client.query("SELECT * FROM sites", (error, results) => {
+    if (error) {
+      throw error;
+    }
+
+    response.status(200).json(results.rows);
+  });
+});
+
+export default router;
