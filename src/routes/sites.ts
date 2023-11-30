cd26b25fd792f43549fe7b924dfd6f640d8b7cfc
Rafael Olivares <olrafael@mailfence.com>
2023-11-29 22:43:41 -0300

    Revert "Order site list by mentions count"
    This reverts commit 394690165e5968d8af6d06954296045a3eaaf5fd.

--- src/routes/sites.ts
+++ src/routes/sites.ts
@@ -5,16 +5,13 @@ import { client } from "../config";
 const router = express.Router();
 
 router.get("/sites", (request: Request, response: Response) => {
-  client.query(
-    "SELECT site FROM mentions GROUP BY site ORDER BY COUNT(*) DESC;",
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
+  client.query("SELECT * FROM sites ORDER BY site ", (error, results) => {
+    if (error) {
+      throw error;
+    }
 
-      response.status(200).json(results.rows);
-    },
-  );
+    response.status(200).json(results.rows);
+  });
 });
 
 export default router;
